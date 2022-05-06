514466f122c245bedb3482975b7202703712c443
Jacob Hutchinson <jacobhutchinson4@yahoo.com>
2022-05-06 12:10:41 -0600

    trying with the path

--- server/index.js
+++ server/index.js
@@ -11,8 +11,12 @@ app.use(cors())
 
 const {getDateList, addDateList, randomList, deleteDateList, editDate} = require('./controller')
 
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname , '.public')))
 app.use(express.static('node_modules'))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/home.html'))
+})
 
 app.get('/dateList', getDateList)
 app.get('/randomList', randomList)
